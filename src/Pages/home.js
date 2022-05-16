738c09d742ef841109c0c265803b566a6c666999
brunosa91 <brunosantana.engenharia@gmail.com>
2022-05-16 13:26:45 -0300

    estruturado o heigh e width das página home

---
+++ src/Pages/home.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { variable } from "../Style/GlobalStyle.js";
+
+export const Main = styled.main`
+  width: 100%;
+  display: flex;
+  min-height: calc(100vh - 70px);
+  background-image: linear-gradient(
+    to right,
+    ${variable.background},
+    ${variable.backgroundDegrade}
+  );
+`;
+export const Section = styled.section`
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  > p {
+    font-size: 20px;
+    text-align: center;
+    padding: 100px;
+    font-family: ${variable.textStyle};
+  }
+`;
