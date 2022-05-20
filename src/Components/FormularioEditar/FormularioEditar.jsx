4bb431f2c20e7e5c219a6001965bf2ee5f3a4572
brunosa91 <brunosantana.engenharia@gmail.com>
2022-05-20 17:33:25 -0300

    implementado as operações de delete e update no front end

--- src/Components/FormularioEditar/FormularioEditar.jsx
+++ src/Components/FormularioEditar/FormularioEditar.jsx
@@ -1,30 +1,118 @@
-import React from "react";
+import React, { useEffect } from "react";
 import * as S from "./formularioEditar.js";
 import Input from "../../Components/Input/Input.jsx";
 import { useState } from "react";
 import { Api } from "../../Services/Api.js";
 // import { useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
+
 import { Button } from "../Button/button.js";
 
-function Formulario() {
+function FormularioEditar({ id }) {
+  const [dados, setDados] = useState({});
+  useEffect(() => {
+    Api.get("/" + id)
+      .then((response) => {
+        setDados(response.data);
+        console.log(response.data);
+      })
+      .catch((erro) => console.log(erro));
+  }, [id]);
+
+  function handleUpdate(e) {
+    e.preventDefault();
+    console.log(dados);
+    Api.put("", dados)
+      .then((response) => {
+        console.log(response);
+      })
+
+      .catch((err) => console.log(err));
+  }
+
+  function handleDelete(e) {
+    e.preventDefault();
+    Api.delete("" + id, dados)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function handleOnChange(e) {
+    setDados({ ...dados, [e.target.name]: e.target.value });
+  }
   return (
     <S.Form>
-      <Input name="NOME" />
-      <Input name="CRM" />
-      <Input name="TELEFONE" />
-      <Input name="CELULAR" />
-      <Input name="CEP" />
-      <Input name="LOGRADOURO" />
-      <Input name="NÚMERO" />
-      <Input name="COMPLEMENTO" />
-      <Input name="CIDADE" />
-      <Input name="ESTADO" />
-      <Input name="ESPECIALIDADE" />
-      <Button>EDITAR</Button>
-      <Button>DELETAR</Button>
+      <Input
+        name="nome"
+        type="text"
+        onChange={handleOnChange}
+        value={dados.nome}
+      />
+      <Input
+        name="crm"
+        type="text"
+        onChange={handleOnChange}
+        value={dados.crm}
+      />
+      <Input
+        name="telefone"
+        type="text"
+        onChange={handleOnChange}
+        value={dados.telefone}
+      />
+      <Input
+        name="celular"
+        type="text"
+        onChange={handleOnChange}
+        value={dados.celular}
+      />
+      <Input
+        name="cep"
+        type="text"
+        onChange={handleOnChange}
+        value={dados.cep}
+      />
+      <Input
+        name="logradouro"
+        type="text"
+        onChange={handleOnChange}
+        value={dados.logradouro}
+      />
+      <Input
+        name="numero"
+        type="text"
+        onChange={handleOnChange}
+        value={dados.numero}
+      />
+      <Input
+        name="complemento"
+        type="text"
+        onChange={handleOnChange}
+        value={dados.complemento}
+      />
+      <Input
+        name="cidade"
+        type="text"
+        onChange={handleOnChange}
+        value={dados.cidade}
+      />
+      <Input
+        name="estado"
+        type="text"
+        onChange={handleOnChange}
+        value={dados.estado}
+      />
+      <Input
+        name="especialidade"
+        type="text"
+        onChange={handleOnChange}
+        value={dados.especialidade}
+      />
+      <button onClick={handleUpdate}>EDITAR</button>
+      <button onClick={handleDelete}>DELETAR</button>
     </S.Form>
   );
 }
 
-export default Formulario;
+export default FormularioEditar;
